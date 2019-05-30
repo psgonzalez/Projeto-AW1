bb973e4da0b4dbdf5324ca21a06c0c93af983cd7
Paula Gonzalez <psgnzalez@gmail.com>
2019-05-30 17:15:25 -0300

    Página de login pronta. adicionado animação para div esqueceu-senha

--- js/entrar.js
+++ js/entrar.js
@@ -1,3 +1,13 @@
 $("#esqueceusenha").click(function() {
     $("#esqueceu-senha").show()
 });
+
+//Animação para div esqueceu-senha
+
+$('#esqueceusenha').on('click', function() {
+    const div = $('#esqueceu-senha').position().top;
+
+    $('html, body').animate({
+        scrollTop: div
+    }, 1200);
+});
