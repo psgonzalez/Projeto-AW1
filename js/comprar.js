2de850c0fa63b533017baa332ceedbfad81d64ee
psgonzalez <psgnzalez@gmail.com>
2019-06-12 10:49:43 -0300

    adicionado div de frete na página de compra

---
+++ js/comprar.js
@@ -0,0 +1,58 @@
+$(document).ready(function() {
+
+    var end = document.getElementById("endereco");
+
+    function limpa_formulário_cep() {
+        // Limpa valores do formulário de cep.
+        end.textContent = "";
+        
+    }
+    
+    //Quando o campo cep perde o foco.
+    $("#cep-id").blur(function() {
+
+        //Nova variável "cep" somente com dígitos.
+        var cep = $(this).val().replace(/\D/g, '');
+
+        //Verifica se campo cep possui valor informado.
+        if (cep != "") {
+
+            //Expressão regular para validar o CEP.
+            var validacep = /^[0-9]{8}$/;
+
+            //Valida o formato do CEP.
+            if(validacep.test(cep)) {
+
+                
+                
+                //Consulta o webservice viacep.com.br/
+                $.getJSON("https://viacep.com.br/ws/"+ cep +"/json/?callback=?", function(dados) {
+
+                    if (!("erro" in dados)) {
+                        //Atualiza o campo com o valor da consulta.
+                        end.textContent = dados.logradouro;
+                        
+                    } //end if.
+                    else {
+                        //CEP pesquisado não foi encontrado.
+                        limpa_formulário_cep();
+                        alert("CEP não encontrado.");
+                    }
+                });
+            } //end if.
+            else {
+                //cep é inválido.
+                limpa_formulário_cep();
+                alert("Formato de CEP inválido.");
+            }
+        } //end if.
+        else {
+            //cep sem valor, limpa formulário.
+            limpa_formulário_cep();
+        }
+    });
+});
+
+$("#botao-frete").click(function() {
+    $("#section4").slideDown(500)
+});
